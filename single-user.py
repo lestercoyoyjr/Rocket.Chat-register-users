e11ab535afbd4c76c2b3d5ff4db4ecca46405601
lestercoyoy <lester_cch@hotmail.com>
2023-09-20 19:52:02 -0600

    ADD
    Single user file. This in order to make it a menu where you can choose different options

---
+++ single-user.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Rocket.Chat API endpoint for creating a user
+url = "https://lestercch.rocket.chat/api/v1/users.create"
+
+# Headers with authentication tokens
+headers = {
+    "X-Auth-Token": os.getenv("X-Auth-Token"),
+    "X-User-Id": os.getenv("X-User-Id"),
+    "Content-Type": "application/json",
+}
+
+# User data to create
+user_data = {
+    "name": "test-agent11",
+    "username": "test.agent11",
+    "email": "test.agent11@gmail.com",
+    "password": "1234",
+}
+
+try:
+    # Send POST request to create the user
+    response = requests.post(url, headers=headers, json=user_data)
+
+    # Check the response status code
+    if response.status_code == 200:
+        print("User created successfully!")
+    else:
+        print(f"Failed to create user. Status code: {response.status_code}")
+        print(response.text)
+
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
